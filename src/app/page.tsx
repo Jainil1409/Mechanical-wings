2769e7d3b230966f4b504547b3e856a326927915
Jainil1409 <Jainil1vyas1414@gmail.com>
2026-04-14 09:53:13 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -498,8 +498,8 @@ export default function Home() {
 
           {/* Scrolling Brands */}
           <div className="relative">
-            <div className="absolute left-0 top-0 bottom-0 w-20 bg-linear-to-r from-white to-transparent z-10" />
-            <div className="absolute right-0 top-0 bottom-0 w-20 bg-linear-to-l from-white to-transparent z-10" />
+            
+            
             <motion.div
               className="flex gap-8"
               animate={{ x: [0, -1200] }}
